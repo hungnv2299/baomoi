a30bb8f3907649b2fe7fb4c678e9722108c2a98d
Hung2299 <hungkoj9x@gmail.com>
2021-08-19 09:53:49 +0700

    layout completed

---
+++ app/src/main/java/com/hung/baomoi/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,7 @@
+package com.hung.baomoi.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.hung.baomoi.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+}
---
+++ app/src/main/java/com/hung/baomoi/ui/fragments/SavedNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.hung.baomoi.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.hung.baomoi.R
+
+class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
+}
